414af397f02933a0e0e73ca9e329982aa5855bc0
Phil Broderick <philbroderick12@gmail.com>
2020-12-28 17:33:54 +0000

    Include missing files failing build

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+
+namespace Portfolio.Pages
+{
+    public class Contact : PageModel
+    {
+        public void OnGet() => Page();
+        
+        [BindProperty] 
+        public ContactSubmission ContactSubmission { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            
+            // send email
+            return null;
+        }
+    }
+}
