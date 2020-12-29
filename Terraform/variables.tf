b5ed0e262c006f6046f45117dfaccfefb75b7781
Phil Broderick <philbroderick12@gmail.com>
2020-12-29 17:00:30 +0000

    Add initial terraform resources

---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+﻿variable "location" {
+  description = "location to deploy resources into"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "resource group name to deploy resources into"
+  type        = string
+}
