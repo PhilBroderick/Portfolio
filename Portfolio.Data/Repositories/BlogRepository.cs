69ea0cc3103e109b3800f049a5388e484028a62d
Phil Broderick <philbroderick12@gmail.com>
2021-01-01 15:50:41 +0000

    Add data class library

---
+++ Portfolio.Data/Repositories/BlogRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Portfolio.Core.Interfaces;
+using Portfolio.Core.ServiceModels;
+
+namespace Portfolio.Data.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        public async Task<IEnumerable<BlogItem>> GetBlogs(int numOfBlogs)
+        {
+            // stubbed out data until persistence implemented
+            var result = new List<BlogItem>();
+            var start = new DateTime(2000, 1, 1);
+            var rng = new Random(Guid.NewGuid().GetHashCode());
+            var range = (DateTime.Today - start).Days;
+            for (int i = 0; i < numOfBlogs; i++)
+            {
+                result.Add(new BlogItem
+                {
+                    Created = start.AddDays(rng.Next(range)),
+                    Title = $"Title: {i + 1}"
+                });
+            }
+
+            return result;
+        }
+
+        public Task<BlogItem> CreateNewBlog(string title, string content)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
