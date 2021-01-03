a6885d2dcd2ca0c1979dd11a5181c8a6d74a1427
Phil Broderick <philbroderick12@gmail.com>
2021-01-03 17:32:52 +0000

    Remove JWT authentication

--- Portfolio.Core/Interfaces/Repositories/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Portfolio.Core.Interfaces.Repositories
-{
-    public interface IAuthRepository
-    {
-        Task<bool> ValidateUser(string username, string password);
-    }
-}
--- Portfolio.Core/ServiceModels/LoginModels.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Portfolio.Core.ServiceModels
-{
-    public record LoginRequest(string Username, string Password);
-
-    public record LoginResponse(string Username, string Token, bool IsError = false, string ErrorMessage = "");
-}
