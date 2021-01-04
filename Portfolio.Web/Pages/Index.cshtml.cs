6dafb54ceff43dda1b579c3b6eb0c6a0d056bc21
Phil Broderick <philbroderick12@gmail.com>
2021-01-04 17:00:46 +0000

    Refactor all pages to use grid system

--- Portfolio.Web/Pages/Index.cshtml.cs
+++ Portfolio.Web/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Portfolio.Models.Content;
 
 namespace Portfolio.Pages
 {
@@ -12,9 +13,15 @@ namespace Portfolio.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public static IndexContent Content;
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Content = new IndexContent(
+                "Hi, I'm Phil Broderick",
+                "Full stack developer specialising in Azure and DevOps practices",
+                "https://pbportfolio.blob.core.windows.net/images/photo-1600267185393-e158a98703de.jpg");
         }
 
         public void OnGet()
