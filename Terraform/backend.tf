bcf74f0d2be26570b77f45af86ba2a8352d45f33
PhilBroderick <philbroderick12@gmail.com>
2021-01-24 15:04:24 +0000

    add required resources and create infra and deploy jobs to actions file

---
+++ Terraform/backend.tf
@@ -0,0 +1,8 @@
+﻿terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraformstaterg"
+    storage_account_name = "terraformstatepb"
+    container_name       = "portfolio"
+    key                  = "portfolio.terraform.tfstate"
+  }
+}
